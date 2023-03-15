86beeb3c83b665ee3dcd64a0f2bca4f2116db688
Fez06 <mattiafezzardi1993@gmail.com>
2023-03-15 17:07:46 +0100

    cercando di utilizzare il v-if milestone 1

--- main.js
+++ main.js
@@ -1,1 +1,34 @@
 'use strict';
+
+const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+            todoItems: [
+                {
+                    text:'fare la spesa',
+                    bool: false
+                },
+                {
+                    text:'fare una corsa',
+                    bool: false
+                },
+                {
+                    text:'leggere 20 pagine',
+                    bool: false
+                },
+                {
+                    text:'passare da gigi',
+                    bool: true
+                }
+            ]
+        }
+    },
+
+    methods: {
+        done(element) {
+            element.classList.add('marked')
+        }
+    }
+}).mount('#App')
